58172a4a03345ab1fb8e395404600edb15bc4a31
Herman <hermansyah_@live.com>
2025-01-21 13:49:37 +0700

    nav script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,15 +75,7 @@
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-    var dropdowns = document.querySelectorAll('.dropdown-toggle');
-    dropdowns.forEach(function (dropdown) {
-        new bootstrap.Dropdown(dropdown);
-    });
-});
-
-    </script>
+   
         
 
       
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -84,3 +84,13 @@
         </div>
     </div>
 </nav>
+
+<script>
+        document.addEventListener('DOMContentLoaded', function () {
+    var dropdowns = document.querySelectorAll('.dropdown-toggle');
+    dropdowns.forEach(function (dropdown) {
+        new bootstrap.Dropdown(dropdown);
+    });
+});
+
+    </script>
