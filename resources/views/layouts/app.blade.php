0060aed0f28bba462f931d5a0ea1fb2cd7be5b4c
Herman <hermansyah_@live.com>
2025-01-21 12:28:51 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,6 @@
 
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet"> -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+4mJpzt4xzg+nEr39QPGPxh5X1+mB" crossorigin="anonymous"></script>
 
         <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
         <!-- Leaflet JS -->
@@ -44,6 +43,8 @@
 
         <!-- Custom CSS -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+4mJpzt4xzg+nEr39QPGPxh5X1+mB" crossorigin="anonymous"></script>
+
 
         <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script> -->
     </head>
