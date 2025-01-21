db40e93c1698da24c0c6f3403cfe0d02f38eafd2
Herman <hermansyah_@live.com>
2025-01-21 12:12:46 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,8 @@
         <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
         <!-- Leaflet JS -->
         <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+        
+        
         <!-- Tambahkan ini di file layout utama -->
 
 
@@ -40,6 +41,8 @@
 
         <!-- Custom CSS -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </head>
     <body class="font-sans bg-light">
         <div class="min-vh-100 d-flex flex-column">
